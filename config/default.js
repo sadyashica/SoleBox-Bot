7b14f39b46041d0ee1ac8d79a97fae12a502aed8
Sad Yashica <sadyashica@gmail.com>
2018-08-25 17:35:21 +0200

    first part

---
+++ config/default.js
@@ -0,0 +1,14 @@
+const options = {
+  product: {
+    url: 'https://www.solebox.com/Footwear/Running/ACG-Ruckel-Ridge.html',
+    size: '11'
+  },
+  customer: {
+    email: 'username@gmail.com',
+    password: 'passwordN*'
+  },
+  proxy: 'http://localhost:8888'
+};
+
+// # export for use elsewhere
+module.exports = options;
---
+++ index.js
@@ -0,0 +1,9 @@
+// # Application Bootup
+// boots up the application
+
+// defines that javascript should be executed in 'strict mode' (ECMAScript 5)
+'use strict';
+
+// # Initialize CLI
+// imports the Command Line Interface (CLI) in order to make the application work
+module.exports = require('./lib/cli/index.js');
